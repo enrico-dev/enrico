c95210d150b90af295d221285d90f47c1b828f79
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-04-13 14:01:55 -0500

    WIP: Began single-physics tests for OpenMC

---
+++ tests/src/openmc_api/main.cpp
@@ -0,0 +1,11 @@
+#include "drivers.h"
+#include "mpi.h"
+
+int main(int argc, char* argv[])
+{
+  MPI_Init(&argc, &argv);
+  OpenmcDriver driver(MPI_COMM_WORLD);
+  driver.initStep();
+  driver.solveStep();
+  driver.finalizeStep();
+}
