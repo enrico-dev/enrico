0ab60fc190226584f01860a34612ca4dd17bee42
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-04-20 09:35:03 -0500

    Omit extra check for MPI_Finalized in test_openmc_api

--- tests/openmc_api/main.cpp
+++ tests/openmc_api/main.cpp
@@ -5,17 +5,13 @@ int main(int argc, char* argv[])
 {
   MPI_Init(&argc, &argv);
 
-  auto * testDriver = new OpenmcDriver(MPI_COMM_WORLD);
+  auto *testDriver = new OpenmcDriver(MPI_COMM_WORLD);
   testDriver->initStep();
   testDriver->solveStep();
   testDriver->finalizeStep();
   delete testDriver;
 
-  // HDF5 cleanup occurs in OpenmcDriver's destructor, so MPI_Finalize may have already been called
-  int isFinalized;
-  MPI_Finalized(&isFinalized);
-  if (!isFinalized)
-    MPI_Finalize();
+  MPI_Finalize();
 
   return 0;
 }
