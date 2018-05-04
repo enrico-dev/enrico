7dd966706d52e835abd7932f50fdf8bca6d40447
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-05-04 10:34:16 -0500

    WIP: Finished setting up comm splits, but not tested

--- tests/coupled_driver/main.cpp
+++ tests/coupled_driver/main.cpp
@@ -1,6 +1,7 @@
 #include "drivers.h"
 #include "mpi.h"
 #include <algorithm>
+#include <vector>
 
 struct ProcInfo{
   MPI_Comm comm = MPI_COMM_NULL;
@@ -15,44 +16,76 @@ int main(int argc, char* argv[])
 {
   MPI_Init(&argc, &argv);
 
-  ProcInfo nekGlobal;
-  MPI_Comm_dup(MPI_COMM_WORLD, &nekGlobal.comm);
-  MPI_Comm_group(nekGlobal.comm, &nekGlobal.group);
-  MPI_Comm_rank(nekGlobal.comm, &nekGlobal.rank);
-  MPI_Comm_size(nekGlobal.comm, &nekGlobal.size);
+  //===========================================================================
+  // ProcInfo world: A dup of MPI_COMM_WORLD
+  //===========================================================================
 
-  ProcInfo nekNode;
-  MPI_Comm_split_type(nekGlobal.comm, MPI_COMM_TYPE_SHARED, nekGlobal.rank, MPI_INFO_NULL, &nekNode.comm);
-  MPI_Comm_group(nekNode.comm, &nekNode.group);
-  MPI_Comm_rank(nekNode.comm, &nekNode.rank);
-  MPI_Comm_size(nekNode.comm, &nekNode.size);
+  ProcInfo world;
+  MPI_Comm_dup(MPI_COMM_WORLD, &world.comm);
+  MPI_Comm_group(world.comm, &world.group);
+  MPI_Comm_rank(world.comm, &world.rank);
+  MPI_Comm_size(world.comm, &world.size);
 
-  ProcInfo openmcNode;
-  MPI_Group_incl(nekNode.group, 1, {0}, &openmcNode.group);
-  MPI_Comm_create(nekNode.comm, openmcNode.group, &openmcNode.comm);
-  MPI_Comm_rank(openmcNode.comm, &openmcNode.rank);
-  MPI_Comm_size(openmcNode.comm, &openmcNode.size);
+  //===========================================================================
+  // ProcInfo allProcsInMyShmem: Use MPI_comm_split_type to get a new comm in
+  // every shmem region.  All procs in a shmem region will belong to the same
+  // comm.
+  //
+  // The shmem region is intended to be a node; the behavior has been
+  // untested for more unusual architectures.
+  //===========================================================================
 
-  ProcInfo openmcGlobal;
-  int transRanksIn[] = {MPI_PROC_NULL}, transRanksOut[] = {MPI_PROC_NULL};
-  if (openmcNode.group != MPI_GROUP_NULL) {
-    transRanksIn[0] = MPI_PROC_NULL;
-    MPI_Group_translate_ranks(openmcNode.group, 1, transRanksIn, nekGlobal.group, transRanksOut);
+  ProcInfo allProcsInMyShmem;
+  MPI_Comm_split_type(world.comm, MPI_COMM_TYPE_SHARED, world.rank, MPI_INFO_NULL,
+                      &allProcsInMyShmem.comm);
+  MPI_Comm_group(allProcsInMyShmem.comm, &allProcsInMyShmem.group);
+  MPI_Comm_rank(allProcsInMyShmem.comm, &allProcsInMyShmem.rank);
+  MPI_Comm_size(allProcsInMyShmem.comm, &allProcsInMyShmem.size);
+
+  //===========================================================================
+  // ProcInfo oneProcInMyShmem: In each group from allProcsInMyShmem, use
+  // MPI_group_incl to select one proc.  There will be a different one of these
+  // groups in every shmem domain.  Most procs will not be in these groups.
+  //===========================================================================
+
+  ProcInfo oneProcInMyShmem;
+  MPI_Group_incl(allProcsInMyShmem.group, 1, {0}, &oneProcInMyShmem.group);
+  MPI_Comm_create(allProcsInMyShmem.comm, oneProcInMyShmem.group, &oneProcInMyShmem.comm);
+  MPI_Comm_rank(oneProcInMyShmem.comm, &oneProcInMyShmem.rank);
+  MPI_Comm_size(oneProcInMyShmem.comm, &oneProcInMyShmem.size);
+
+  //===========================================================================
+  // ProcInfo oneProcInAllShmems: Collect all the procs in all groups from
+  // oneProcInMyShmem.  Create a single new group with one proc from every
+  // shmem.
+  //===========================================================================
+
+  // First, translate each rank in a oneProcInMyShmem group to a rank in world.
+  // The ranks that aren't in a oneProcInMyShmem group will be translated to
+  // MPI_PROC_NULL.
+  int myTransRankOut = MPI_PROC_NULL;
+  int myTransRankIn = oneProcInMyShmem.group != MPI_GROUP_NULL ? oneProcInMyShmem.rank : MPI_PROC_NULL;
+  if (oneProcInMyShmem.group != MPI_GROUP_NULL)
+    MPI_Group_translate_ranks(oneProcInMyShmem.group, 1, &myTransRankIn, world.group, &myTransRankOut));
+
+  // Next, gather all the translated (world) ranks from *all* procs.
+  std::vector<int> allTransRanks(world.size, MPI_PROC_NULL);
+  MPI_Allgather(&myTransRankOut, 1, MPI_INT, allTransRanks.data(), 1, MPI_INT, world.comm);
+
+  // Next, remove the invalid ranks.  This will leave us with only the procs
+  // that are in a oneProcInMyShmem group
+  auto validRanks = allTransRanks;
+  auto validEnd = std::remove_if(validRanks.begin(), validRanks.end(), [](int v){return v == MPI_PROC_NULL;});
+  validRanks.erase(validRanks.begin(), validEnd);
+
+  // Finally, make a new group and comm from the valid ranks.
+  ProcInfo oneProcInAllShmems;
+  MPI_Group_incl(world.comm, static_cast<int>(validRanks.size()), validRanks.data(), &oneProcInAllShmems.group);
+  MPI_Comm_create(world.comm, oneProcInAllShmems.group, &oneProcInAllShmems.comm);
+  if (oneProcInAllShmems.comm == MPI_COMM_NULL) {
+    MPI_Comm_rank(oneProcInAllShmems.comm, &oneProcInAllShmems.rank);
+    MPI_Comm_size(oneProcInAllShmems.comm, &oneProcInAllShmems.size);
   }
-  auto *translatedRanks = new int[nekGlobal.size];
-  MPI_Allgather(transRanksOut, 1, MPI_INT, translatedRanks, 1, MPI_INT, nekGlobal.comm);
-  if (openmcNode.group != MPI_GROUP_NULL) {
-
-    // ROR: Can I get this array with an iterator?
-    auto numValidRanks = std::count_if(translatedRanks, translatedRanks+nekGlobal.size, isValidRank);
-    auto *validRanks = new int[numValidRanks];
-
-
-  }
-
-
-  delete []translatedRanks;
-
 
   MPI_Finalize();
   return 0;
