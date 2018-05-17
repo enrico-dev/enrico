7d0f26c1d2614f9f2b68a40c4f4b82a23e42a729
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-05-17 15:24:12 -0500

    Put OpenmcNekDriver in its own scope. Destroyed before MPI_Finalize.

--- tests/coupled_driver/main.cpp
+++ tests/coupled_driver/main.cpp
@@ -12,18 +12,17 @@ int main(int argc, char *argv[]) {
   MPI_Comm coupledComm = MPI_COMM_WORLD;
   MPI_Comm nekComm = MPI_COMM_WORLD;
 
-  auto *testDriver =
-      new OpenmcNekDriver(argc, argv, coupledComm, openmcComm, nekComm);
+  {
+    OpenmcNekDriver testDriver(argc, argv, coupledComm, openmcComm, nekComm);
 
-  testDriver->openmcDriver.initStep();
-  testDriver->openmcDriver.solveStep();
-  testDriver->openmcDriver.finalizeStep();
+    testDriver.openmcDriver.initStep();
+    testDriver.openmcDriver.solveStep();
+    testDriver.openmcDriver.finalizeStep();
 
-  testDriver->nekDriver.initStep();
-  testDriver->nekDriver.solveStep();
-  testDriver->nekDriver.finalizeStep();
-
-  delete testDriver;
+    testDriver.nekDriver.initStep();
+    testDriver.nekDriver.solveStep();
+    testDriver.nekDriver.finalizeStep();
+  }
 
   MPI_Finalize();
 }
