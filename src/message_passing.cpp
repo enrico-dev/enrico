67912b4de679cad6d1b659c61241dfb4c59cd0fe
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-05-18 09:59:18 -0500

    Commented getInterNodeSubComm

--- src/message_passing.cpp
+++ src/message_passing.cpp
@@ -1,6 +1,24 @@
 #include "message_passing.h"
 #include "mpi.h"
 
+/**
+ * @brief Splits a given MPI comunicator into a new comm with a specified number of procs in every node.
+ *
+ * Tne new comm (*subComm*) will span **all** nodes in the original comm (*superComm*).  This will work correctly even
+ * if *subComm* spans only a single node.
+ *
+ * Depending on the calling proc, *subComm* will be one of two values:
+ *   - If the calling proc is within the new desired comm (the comm with the given number of *procsPerNode*), then
+ *     *subComm* will be a valid comm.
+ *   - If the calling proc is not within the new desired comm, then *subComm* will be `MPI_COMM_NULL`.
+ * The caller must then make use of *subComm* with proper value checks.  For example, subsequent MPI operations on
+ * *subComm* may need to check if `subComm != MPI_COMM_NULL`.
+ *
+ * @param superComm An existing communicator that will be split
+ * @param procsPerNode The number of MPI procs per node in the new communicator, *subComm*
+ * @param subComm A new communicator with either the given number of procs per node *or* `MPI_COMM_NULL`, depending on
+ *                whether the calling proc is in the desired comm.
+ */
 void getInternodeSubComm(MPI_Comm superComm, int procsPerNode, MPI_Comm *subComm) {
 
   // superCommRank is used as the "key" to retain ordering in the comm splits.
