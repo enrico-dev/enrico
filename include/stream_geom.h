ca077ed17af6e10b982895ceb03b7dcf08f82533
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-08-27 14:43:00 -0500

    Updated comments for stream_const.h and stream_geom.h

--- include/stream_geom.h
+++ include/stream_geom.h
@@ -1,15 +1,15 @@
+//! \file stream_geom.h
+//! For describing STREAM geometries
 #ifndef STREAM_STREAM_GEOM_H
 #define STREAM_STREAM_GEOM_H
 
 namespace stream {
 
-/** Describes an (x,y,z) coordinate in 3D space
- * @var x
- * @var y
- * @var z
- */
+//! Describes an (x,y,z) coordinate in 3D space.
 struct Position {
-  double x, y, z;
+  double x;  //!< x-coordinate
+  double y;  //!< y-coordinate
+  double z;  //!< z-coordinate
 };
 
 } // namespace stream
