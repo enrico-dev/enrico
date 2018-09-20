70e5252cfae0e7619b016efb5bcdcd45a7b0ed33
Ron Rahaman <rahaman@mcs.anl.gov>
2018-09-20 16:20:05 -0500

    Moved all of nek_interface to libnek5000

--- src/openmc_nek_driver.cpp
+++ src/openmc_nek_driver.cpp
@@ -17,12 +17,6 @@ OpenmcNekDriver::OpenmcNekDriver(int argc, char** argv, MPI_Comm coupled_comm,
   n_global_elem_ = nek_driver_.active() ? nek_driver_.nelgt_ : 0;
 
   init_mpi_datatypes();
-  for (int i=1; i <= n_local_elem_; ++i) {
-    std::cout << i
-              << nek_driver_.get_local_elem_centroid(i).x << " "
-              << nek_driver_.get_local_elem_centroid(i).y << " "
-              << nek_driver_.get_local_elem_centroid(i).z << std::endl;
-  }
   init_mappings();
   init_tallies();
   init_volumes();
