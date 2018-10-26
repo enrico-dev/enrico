85e681a91baaffdad7f2de483f84a2bb17a861a0
Ronald Rahaman <rahaman@mcs.anl.gov>
2018-10-26 11:08:32 -0500

    Cleaned-up sums in get_local_elem_volume, get_local_elem_temperature

--- vendor/nek5000/core/nek_interface.f
+++ vendor/nek5000/core/nek_interface.f
@@ -117,10 +117,7 @@
           integer :: k
 
           if (local_elem <= nelt) then
-            volume = 0
-            do k = 1, nx1 * ny1 * nz1
-              volume = volume + bm1(k, 1, 1, local_elem)
-            enddo
+            volume = sum(bm1(1:nx1,1:ny1,1:nz1,local_elem))
             ierr = 0
           else
             ierr = 1
@@ -134,22 +131,12 @@
           integer(C_INT) :: ierr
           integer :: k
 
-          double precision :: ssum1, ssum2
-
           if (local_elem <= nelt) then
-
-            ssum1 = 0
-            ssum2 = 0
-
-            do k = 1, nx1 * ny1 * nz1
-              ssum1 = ssum1 + vtrans(k,1,1,local_elem,2) *
-     &             bm1(k,1,1,local_elem) * t(k,1,1,local_elem,1)
-              ssum2 = ssum2 + vtrans(k,1,1,local_elem,2) *
-     &             bm1(k,1,1,local_elem)
-            enddo
-
-            temperature = ssum1 / ssum2
-
+            temperature = sum(vtrans(1:nx1,1:ny1,1:nz1,local_elem,2)
+     &                        * bm1(1:nx1,1:ny1,1:nz1,local_elem)
+     &                        * t(1:nx1,1:ny1,1:nz1,local_elem,1))
+     &                    / sum(vtrans(1:nx1,1:ny1,1:nz1,local_elem,2)
+     &                          * bm1(1:nx1,1:ny1,1:nz1,local_elem))
             ierr = 0
           else
             ierr = 1
