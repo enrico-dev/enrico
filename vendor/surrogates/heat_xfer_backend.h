a0a53e13dbb1986de10f492403a4cedc1a638de8
Ronald Rahaman <rahaman@mcs.anl.gov>
2019-05-10 11:24:56 -0500

    Moved surrogates/iapws.cpp to its own directory w/header and targets

--- vendor/surrogates/heat_xfer_backend.h
+++ vendor/surrogates/heat_xfer_backend.h
@@ -5,7 +5,4 @@ void solve_steady_nonlin(double *source, double T_co, double *r_grid_fuel,
   double *r_grid_clad, int n_fuel_rings, int n_clad_rings,
   double tol, double *T);
 
-// From IAPWS
-double nu1(double p, double T);
-
 #endif // MAGNOLIA_HEAT_XFER_BACKEND_H
