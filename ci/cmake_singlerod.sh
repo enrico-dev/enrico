18560c3f4b2a9c2fea0177173585e96ffe44f043
Ron Rahaman <rahaman@mcs.anl.gov>
2019-05-13 09:46:58 -0500

    Added .travis.yml

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -3,5 +3,5 @@ set -ex
 
 mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
-cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx _DCMAKE_Fortran_COMPILER=mpif90 \
+cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_Fortran_COMPILER=mpif90 \
   -DUSR_LOC=../ ../../../..
