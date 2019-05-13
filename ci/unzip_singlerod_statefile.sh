f92d15600a2022e2b2f950b789a4f04dc86dbf27
Ron Rahaman <rahaman@mcs.anl.gov>
2019-05-13 13:36:24 -0500

    Travis checks for cached XS; eliminates some other unneeded steps

--- ci/unzip_singlerod_statefile.sh
+++ ci/unzip_singlerod_statefile.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-cd tests/singlerod/short
-gunzip -f rodcht.run01.gz
-cp rodcht.run01 nek5000/
-cp rodcht.run01 openmc_nek5000/
+if [ "$MODE" = "nek5000" ] || [ "$MODE" = "openmc_nek5000" ]; then
+  cd tests/singlerod/short
+  gunzip -f rodcht.run01.gz
+  cp rodcht.run01 "$MODE/"
+fi
