4d25350ca9050b691375cfc1da11413e1ee7a2aa
Ronald Rahaman <rahaman@cs.uchicago.edu>
2019-05-15 09:15:34 -0500

    Travis correctly finds cached cross sections

--- ci/download_xs.sh
+++ ci/download_xs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ ! -d $HOME/endf71_multitemp/ ]; then
+if [ ! -e $HOME/endf71_multitemp/cross_sections.xml ]; then
   cd $HOME
   wget https://anl.box.com/shared/static/46osfq56h4bd68r6e6pbhupsk4gbnvj2.xz -O - | tar -xvJ
 fi
