344095c42aff7ee343de5e5f24507f369d06d46c
Ron Rahaman <rahaman@mcs.anl.gov>
2019-10-09 10:59:18 -0500

    Updates to patch_singlerod_rea.sh

--- ci/patch_singlerod_rea.sh
+++ ci/patch_singlerod_rea.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
-if [ "$MODE" = "openmc_nek5000" ]; then
+if [[ -z "$MODE" ]] || [[ "$MODE" == "openmc_nek5000" ]] ; then
   cd tests/singlerod/short
   patch -N rodcht.rea ci_config/rodcht_rea.diff || true
   patch -N SIZE ci_config/SIZE.diff || true
