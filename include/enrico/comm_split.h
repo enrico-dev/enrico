ba4a3c41f9ad8ec0061fe2d184f076da425ae58c
Ron Rahaman <rahaman@mcs.anl.gov>
2020-04-29 14:45:52 -0500

    Omitted get_node_comms and commented get_driver_comms

--- include/enrico/comm_split.h
+++ include/enrico/comm_split.h
@@ -8,41 +8,20 @@
 
 namespace enrico {
 
-//! Splits a given MPI comunicator into new inter- and intra-node communicators
+//! Splits a given MPI communicator into new communicators for each single-physics driver
 //!
-//! This will create two new comms, sub_comm and intranode_comm, for the calling proc:
-//! - sub_comm is an internode comm that will span all nodes in the original comm
-//! (super_comm).  It
-//!   will contain procs_per_node procs in each node.
-//! - intranode_comm is an intranode comm that will span a single node.  It will contain
-//! the subset
-//!   of super_comm that are in the node of the calling proc.
-//!
-//! In the above description, "node" refers to a shared-memory region as defined by the
-//! the MPI implementation's MPI_COMM_TYPE_SHARED.  In most use cases, this will be a
-//! node.
-//!
-//! For the calling proc, the new comms (sub_comm and intranode_comm) will be either valid
-//! or invalid depending on these criteria:
-//! - If the calling proc is within the new comm, then the comm will be valid for the
-//! calling proc.
-//! - If the calling proc is not within the new comm, then the comm will be MPI_COMM_NULL
-//! for the
-//!   calling proc.
-//!
-//! The calling proc must then make use of with proper value checks.  For example,
-//! subsequent MPI operations on sub_comm may need to check if `subComm != MPI_COMM_NULL`.
-//!
-//! \param[in] super_comm An existing comm that will be split into sub_comm and
-//! intranode_comm \param[in] procs_per_node The number of procs per node in the new
-//! communicator (sub_comm) \param[out] sub_comm A new internode comm with the desired
-//! number of procs per node \param[out] intranode_comm A new intranode comm wil procs in
-//! the same node
-void get_node_comms(Comm super_comm,
-                    int procs_per_node,
-                    Comm& sub_comm,
-                    Comm& intranode_comm);
-
+//! \param[in] super_comm An existing communicator that will be split
+//! \param[in] num_nodes The desired number of nodes for the each single-physics driver's
+//!            new communicator.  If a value is <= 0, then the respective driver's
+//!            communicator will contain all nodes of super_comm
+//! \param[in] procs_per_node The desired number of procs/node for each single-physics
+//!            driver's new communicator.  If a value is <=, then the respective driver's
+//!            communicator will contain the maximum number of available procs/node
+//! \param[out] driver_comms The newly-created communicators, one for each driver.
+//!             Each is comm active on the calling rank if it's contained by the respective
+//!             driver; and null if not.
+//! \param[out] intranode_comm A new comm that spans the node that the calling rank is in
+//! \param[out] coupling_comm A new comm containing one proc per node.
 void get_driver_comms(Comm super_comm,
                       std::array<int, 2> num_nodes,
                       std::array<int, 2> procs_per_node,
--- src/comm_split.cpp
+++ src/comm_split.cpp
@@ -2,28 +2,6 @@
 
 namespace enrico {
 
-void get_node_comms(Comm super_comm,
-                    int procs_per_node,
-                    Comm& sub_comm,
-                    Comm& intranode_comm)
-{
-  // Each intranode_comm contains all ranks in a given shared memory region (usually node)
-  MPI_Comm icomm;
-  MPI_Comm_split_type(
-    super_comm.comm, MPI_COMM_TYPE_SHARED, super_comm.rank, MPI_INFO_NULL, &icomm);
-  intranode_comm = Comm(icomm);
-
-  // Split the specified number of procs_per_node from the super_comm
-  // We only want the comm where color == 0.  The second comm is destroyed.
-  int color = intranode_comm.rank < procs_per_node ? 0 : 1;
-  MPI_Comm scomm;
-  MPI_Comm_split(super_comm.comm, color, super_comm.rank, &scomm);
-  sub_comm = Comm(scomm);
-  if (color != 0) {
-    sub_comm.free();
-  }
-}
-
 void get_driver_comms(Comm super_comm,
                       std::array<int, 2> num_nodes,
                       std::array<int, 2> procs_per_node,
