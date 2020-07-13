22361341f6da0ddd2f3c51e703c464ad47d0d875
Ron Rahaman <rahaman@mcs.anl.gov>
2020-07-13 17:47:00 -0500

    Adding liblapack to Travis CI build

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -3,5 +3,5 @@ set -ex
 
 mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
-cmake -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_Fortran_COMPILER=mpif90 \
+cmake -DBUILD_SHARED_LIBS=on -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_Fortran_COMPILER=mpif90 \
   -DUSR_LOC=../ ../../../..
