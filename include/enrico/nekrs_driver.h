854652d155c30836ca8e894cc71b089336e3b95f
Ron Rahaman <rahaman@mcs.anl.gov>
2020-07-21 15:20:04 -0500

    Minimal NekrsDriver class

--- include/enrico/nekrs_driver.h
+++ include/enrico/nekrs_driver.h
@@ -1,7 +1,45 @@
 #ifndef ENRICO_SRC_NEKRS_DRIVER_H
 #define ENRICO_SRC_NEKRS_DRIVER_H
 
-class NekRSDriver {};
+#include "mpi.h"
+#include "pugixml.hpp"
+#include "enrico/heat_fluids_driver.h"
+
+namespace enrico {
+class NekRSDriver : public HeatFluidsDriver {
+public:
+  NekRSDriver(MPI_Comm comm, pugi::xml_node node);
+
+  // Default destructor is used; NekRS only runs MPI_Finalize
+  //~NekRSDriver();
+
+  void init_step() override;
+  void solve_step() override;
+  void write_step(int timestep, int iteration) override;
+
+  // TODO: Implement these
+  bool has_coupling_data() const override { return false;}
+  int set_heat_source_at(int32_t local_elem, double heat) override {return -1;}
+  int n_local_elem() const override {return -1;}
+  std::size_t n_global_elem() const override {return -1;};
+
+private:
+  std::string setup_file_;
+  std::string thread_model_;
+  std::string device_number_;
+  double time_;
+  int tstep_;
+
+  // TODO: Implement these
+  std::vector<double> temperature_local() const override {return std::vector<double>{};}
+  std::vector<double> density_local() const override {return std::vector<double>{};}
+  std::vector<int> fluid_mask_local() const override {return std::vector<int>{};}
+  std::vector<Position> centroid_local() const override {return std::vector<Position>{};}
+  std::vector<double> volume_local() const override {return std::vector<double>{};}
+};
+
+}
+
 
 #endif // ENRICO_SRC_NEKRS_DRIVER_H
 
--- src/nekrs_driver.cpp
+++ src/nekrs_driver.cpp
@@ -1,1 +1,53 @@
 #include "enrico/nekrs_driver.h"
+#include "nekrs.hpp"
+
+namespace enrico {
+NekRSDriver::NekRSDriver(MPI_Comm comm, pugi::xml_node node) :
+  HeatFluidsDriver(comm, node) {
+
+  if (active()) {
+    // See vendor/nekRS/src/core/occaDeviceConfig.cpp for valid keys
+    setup_file_ = node.child_value("setup_file");
+    device_number_ = node.child_value("device_number");
+    thread_model_ = node.child_value("thread_model");
+
+    // TODO: Get these values from XML or command line
+    int build_only = 0;
+    int ci_mode = 0;
+    int size_target = 0;
+    int debug = 0;
+
+    std::string cache_dir;
+
+    nekrs::setup(comm, build_only, size_target, ci_mode, cache_dir,
+                 setup_file_, device_number_,
+                 thread_model_);
+  }
+  comm_.Barrier();
+}
+
+void NekRSDriver::init_step() {
+}
+
+void NekRSDriver::solve_step() {
+  const auto start_time = nekrs::startTime();
+  const auto final_time = nekrs::finalTime();
+  const auto dt = nekrs::dt();
+
+  time_ = start_time;
+  tstep_ = 1;
+
+  while ((final_time - time_) / (final_time * dt) > 1e-6){
+    nekrs::runStep(time_, dt, tstep_);
+    time_ += dt;
+    nekrs::udfExecuteStep(time_, tstep_, 0);
+    ++tstep_;
+  }
+}
+
+void NekRSDriver::write_step(int timestep, int iteration) {
+  nekrs::copyToNek(timestep, iteration);
+  nekrs::nekOutfld();
+}
+
+}
