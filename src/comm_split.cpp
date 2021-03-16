b4b319516112fe7704ca7a5bc950dc966709783a
RonRahaman <rahaman@anl.gov>
2021-03-16 14:04:55 -0500

    Get supercomm ranks from gather_subcomm_ranks

--- src/comm_split.cpp
+++ src/comm_split.cpp
@@ -64,7 +64,8 @@ void get_driver_comms(Comm super_comm,
 std::vector<int> gather_subcomm_ranks(const Comm& super, const Comm& sub)
 {
   std::vector<int> ranks(super.size);
-  super.Allgather(&sub.rank, 1, MPI_INT, ranks.data(), 1, MPI_INT);
+  int r = sub.rank != MPI_PROC_NULL ? super.rank : MPI_PROC_NULL;
+  super.Allgather(&r, 1, MPI_INT, ranks.data(), 1, MPI_INT);
   auto new_end =
     std::remove_if(ranks.begin(), ranks.end(), [](int i) { return i == MPI_PROC_NULL; });
   ranks.erase(new_end, ranks.end());
