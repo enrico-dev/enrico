47660592ffc457b3ea5a1e51454f1f770048a5d5
Ronald Rahaman <rahaman@anl.gov>
2021-03-25 09:34:18 -0500

    Update test_singlerod_openmc_nekrs.sh

--- ci/test_singlerod_openmc_nekrs.sh
+++ ci/test_singlerod_openmc_nekrs.sh
@@ -3,5 +3,6 @@ set -ex
 
 cd tests/singlerod/short/openmc_nekrs
 export NEKRS_HOME=$(realpath ../build/install)
+cat rod_short.par
 ../build/install/bin/nrspre rod_short 2
 mpirun -np 2 ../build/install/bin/enrico
