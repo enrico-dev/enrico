12556c055ed277f231109c954d8a66a060be71dd
Kalin Kiesling <krkiesling@gmail.com>
2022-09-29 12:07:06 -0500

    script to generate openmc simple core lattice

--- tests/core/make_openmc_model.py
+++ tests/core/make_openmc_model.py
@@ -4,6 +4,19 @@ from math import pi
 import numpy as np
 import openmc
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-ax', '--assemx', type=int, default=3)
+parser.add_argument('-ay', '--assemy', type=int, default=3)
+parser.add_argument('-px', '--pinx', type=int, default=2)
+parser.add_argument('-py', '--piny', type=int, default=2)
+parser.add_argument('-s', '--skip', action='store_true') # skip corners
+args = parser.parse_args()
+
+# parse input
+na_x = args.assemx
+na_y = args.assemy
+np_x = args.pinx
+np_y = args.piny
 
 # Basic model parameters
 fuel_or = 0.406
@@ -40,49 +53,68 @@ water.add_element('O', 1.0)
 water.add_s_alpha_beta('c_H_in_H2O')
 water.set_density('g/cm3', water_density)
 
+# outer universes
+water_univ = openmc.Universe(cells=[openmc.Cell(fill=water)])
+void_univ = openmc.Universe(cells=[openmc.Cell(name='void')],
+                                    name='null')
+
 # Create cylinders
 fuel_outer = openmc.ZCylinder(r=fuel_or)
 clad_inner = openmc.ZCylinder(r=clad_ir)
 clad_outer = openmc.ZCylinder(r=clad_or)
 
 # make pin
-pin = openmc.model.pin([fuel_outer, clad_inner, clad_outer], [uo2, None, m5, water])
+pin = openmc.model.pin([fuel_outer, clad_inner, clad_outer],
+                       [uo2, void_univ, m5, water])
 
 # make assembly lattice
+assem_pitch_x = pitch * np_x
+assem_pitch_y = pitch * np_y
 assem_lat = openmc.RectLattice()
-assem_lat.lower_left = (-pitch, -pitch, 0.0)
-assem_lat.pitch = (pitch*2, pitch*2, fuel_length)
-assem_lat.universes = np.full((1, 2, 2), pin)
+assem_lat.lower_left = (-assem_pitch_x/2., -assem_pitch_y/2.,)
+assem_lat.pitch = [pitch, pitch]
+assem_lat.universes = np.full((np_y, np_x), pin)
+assem_lat.outer = water_univ
 
 # make assembly
-assembly_region = openmc.rectangular_prism(pitch * 2, pitch * 2, origin=(0, 0, fuel_length/2.0), boundary_type='transmission')
+assembly_region = openmc.rectangular_prism(assem_pitch_x, assem_pitch_y,
+    origin=(0, 0, fuel_length/2.0))
 assembly_cell = openmc.Cell(fill=assem_lat, region=assembly_region)
 assembly = openmc.Universe(cells=[assembly_cell])
 
 # make core lattice
-assem_pitch = pitch * 2
+core_pitch_x = assem_pitch_x * na_x
+core_pitch_y = assem_pitch_y * na_y
 core_lat = openmc.RectLattice()
-core_lat.lower_left = (-assem_pitch, -assem_pitch, 0.0)
-core_lat.pitch = (assem_pitch, assem_pitch, fuel_length)
-core_lat.universes = np.full((1, 3, 3), assembly)
+core_lat.lower_left = (-core_pitch_x/2., -core_pitch_y/2., 0)
+core_lat.pitch = [assem_pitch_x, assem_pitch_y, fuel_length]
+core_lat.universes = np.full((1, na_y, na_x), assembly)
+core_lat.outer = void_univ
+
+# replace corners with assem outer
+if args.skip:
+    core_lat.universes[0][0][0] = water_univ
+    core_lat.universes[0][na_y - 1][0] = water_univ
+    core_lat.universes[0][na_y - 1][na_x - 1] = water_univ
+    core_lat.universes[0][0][na_x - 1] = water_univ
 
 # make core
-core_region = openmc.rectangular_prism(assem_pitch, assem_pitch, origin=(0, 0, fuel_length/2.0), boundary_type='transmission')
-print(core_region.get_surfaces())
+core_region = openmc.rectangular_prism(core_pitch_x, core_pitch_y,
+    origin=(0, 0, fuel_length/2.0), boundary_type='vacuum')
 core = openmc.Cell(fill=core_lat, region=core_region)
 
 model = openmc.model.Model()
 model.geometry = openmc.Geometry([core])
 
+# simulation settings
 model.settings.particles = 1000
 model.settings.inactive = 10
 model.settings.batches = 50
 model.settings.source = openmc.Source(
     space=openmc.stats.Box(
-        (-assem_pitch*3/2., -assem_pitch*3/2., 0.0),
-        (assem_pitch*3/2., assem_pitch*3/2., fuel_length),
-        True
-    )
+        (-core_pitch_x/2., -core_pitch_y/2., 0.0),
+        (core_pitch_x/2., core_pitch_y/2., fuel_length),
+        True)
 )
 model.settings.temperature = {
     'default': 523.15,
