93774d25fe390293e2c364ae6881536d4cb1beb4
Paul Romano <paul.k.romano@gmail.com>
2022-11-28 16:15:50 -0600

    Update data directory for OpenMC cross sections

--- ci/download_xs.sh
+++ ci/download_xs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ ! -e $HOME/endfb71_hdf5/cross_sections.xml ]; then
+if [ ! -e $HOME/endfb-vii.1-hdf5/cross_sections.xml ]; then
   cd $HOME
   wget https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz -O - | tar -xvJ
 fi
